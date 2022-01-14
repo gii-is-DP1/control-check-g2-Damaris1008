3d0cabab1a846cae0c4826403fccab57e9c5033c
Damaris1008 <damarisgomezse@gmail.com>
2022-01-14 12:14:43 +0100

    Control Check de DP resuelto

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,5 +1,67 @@
 package org.springframework.samples.petclinic.feeding;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class FeedingController {
-    
+	
+	private static final String VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM = "feedings/createOrUpdateFeedingForm";
+	
+	private FeedingService feedingService;
+	private PetService petService;
+	
+	@Autowired
+	public FeedingController(FeedingService feedingService, PetService petService) {
+		this.feedingService = feedingService;
+		this.petService = petService;
+	}
+	
+	@GetMapping(value = "/feeding/create")
+	public String initCreationForm(ModelMap model) {
+		Feeding feeding = new Feeding();
+		model.put("feeding", feeding);
+		List<Pet> pets = petService.getAllPets();
+		model.put("pets", pets);
+		List<FeedingType> feedingTypes = feedingService.getAllFeedingTypes();
+		model.put("feedingTypes", feedingTypes);
+		return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/feeding/create")
+	public String processCreationForm(@Valid Feeding feeding, BindingResult result, ModelMap model) {		
+		if (result.hasErrors()) {
+			model.put("feeding", feeding);
+			model.put("feeding", feeding);
+			List<Pet> pets = petService.getAllPets();
+			model.put("pets", pets);
+			List<FeedingType> feedingTypes = feedingService.getAllFeedingTypes();
+			model.put("feedingTypes", feedingTypes);
+			return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+            try{
+            	this.feedingService.save(feeding);
+            }catch(UnfeasibleFeedingException ex){
+                result.rejectValue("pet", "error", "La mascota seleccionada no se le puede asignar el plan de alimentación especificado.");
+                List<Pet> pets = petService.getAllPets();
+    			model.put("pets", pets);
+    			List<FeedingType> feedingTypes = feedingService.getAllFeedingTypes();
+    			model.put("feedingTypes", feedingTypes);
+                return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+            }
+            return "redirect:/welcome";
+		}
+		
+	}
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,21 +2,40 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class FeedingService {
+	
+	private FeedingRepository feedingRepo;
+	
+	@Autowired
+	public FeedingService(FeedingRepository feedingRepo) {
+		this.feedingRepo = feedingRepo;
+	}
+	
+	@Transactional(readOnly = true)
     public List<Feeding> getAll(){
-        return null;
+    	return feedingRepo.findAll();
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        return feedingRepo.findAllFeedingTypes();
     }
 
+    @Transactional(readOnly = true)
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        return feedingRepo.findFeedingTypeByName(typeName);
     }
 
+    @Transactional(rollbackFor = UnfeasibleFeedingException.class)
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+    	if(!p.getPet().getType().equals(p.getFeedingType().getPetType())) {
+    		throw new UnfeasibleFeedingException();
+    	}
+    	return feedingRepo.save(p);       
     }
 
     
